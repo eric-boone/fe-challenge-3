b50238f0192ab6a9ca49dda06ce4dc4611ab2920
Eric Boone <therealeric@gmail.com>
2020-11-02 09:51:24 -0700

    fetching data working

--- bacon/src/App.js
+++ bacon/src/App.js
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
+
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <p>working</p>
-    </div>
-  );
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetch("https://code-challenge.spectrumtoolbox.com/api/restaurants", {
+      headers: {
+        Authorization: "Api-Key q3MNxtfep8Gt",
+      },
+    })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setRestaurants(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+        <p>working</p>
+        {console.log(restaurants)}
+      </div>
+    );
+  }
 }
 
 export default App;
