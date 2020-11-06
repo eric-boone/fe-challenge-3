56c24a65a5d428f74bfec46a92f7a8de3d48c7c6
Eric Boone <therealeric@gmail.com>
2020-11-06 08:06:37 -0700

    refactored sortTable to use state

--- bacon/src/components/Table.js
+++ bacon/src/components/Table.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import stateData from "../data/states.json";
 import Filter from "./Filter";
@@ -6,27 +6,36 @@ import Filter from "./Filter";
 const Table = (props) => {
   const restaurants = props.restaurants;
   const states = stateData;
+  const [rows, setRows] = useState([]);
 
-  const restaurantSort = [...restaurants];
-  restaurantSort.sort((a, b) => {
-    if (a.name < b.name) {
-      return -1;
-    }
-    if (a.name > b.name) {
-      return 1;
-    }
-    return 0;
-  });
+  useEffect(() => {
+    sortTable(restaurants);
+  }, []);
 
-  const rows = restaurantSort.map((restaurants) => (
-    <tr key={restaurants.id}>
-      <td>{restaurants.name}</td>
-      <td>{restaurants.city}</td>
-      <td>{restaurants.state}</td>
-      <td>{restaurants.telephone}</td>
-      <td>{restaurants.genre}</td>
-    </tr>
-  ));
+  function sortTable(restaurants) {
+    const restaurantSort = [...props.restaurants];
+    restaurantSort.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+
+    const rowsSorted = restaurantSort.map((restaurants) => (
+      <tr key={restaurants.id}>
+        <td>{restaurants.name}</td>
+        <td>{restaurants.city}</td>
+        <td>{restaurants.state}</td>
+        <td>{restaurants.telephone}</td>
+        <td>{restaurants.genre}</td>
+      </tr>
+    ));
+
+    setRows(rowsSorted);
+  }
 
   return (
     <div>
@@ -35,13 +44,17 @@ const Table = (props) => {
           <tr>
             <th>name</th>
             <th>city</th>
-            <th>state <Filter states={states} /></th>
+            <th>
+              state <Filter states={states} />
+            </th>
             <th>telephone</th>
             <th>genre</th>
           </tr>
         </thead>
         <tbody>{rows}</tbody>
       </table>
+      {console.log("rows", rows)}
+      {/* {console.log("props", props)} */}
     </div>
   );
 };
