2c6abb67da8e1baa943ad336842c1be07ce9b9ed
Eric Boone <therealeric@gmail.com>
2020-11-11 13:13:46 -0700

    setting up page numbers for pagination component

--- bacon/src/components/Pagination.js
+++ bacon/src/components/Pagination.js
@@ -1,6 +1,12 @@
 import React from "react";
 
-const Pagination = () => {
+const Pagination = ({ rowsPerPage, totalRows, paginate }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalRows / rowsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
   return (
     <div>
       <p>Pagination works</p>
